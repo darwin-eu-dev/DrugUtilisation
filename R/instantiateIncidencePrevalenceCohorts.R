8d5e74ac10bd95a46181a0467a78f5250989e4c0
catalamarti <marticatalasabate@gmail.com>
2022-11-13 21:46:07 +0000

    Update

--- R/instantiateIncidencePrevalenceCohorts.R
+++ R/instantiateIncidencePrevalenceCohorts.R
@@ -18,14 +18,19 @@
 #' codes are considered from the concept sets
 #'
 #' @param cdm 'cdm' object created with CDMConnector::cdm_from_con(). It must
-#' must contain at least 'drug_exposure' and 'concept' table. The 'cdm' object
-#' must contain the 'write_schema' as attribute and  the user should have
+#' must contain at least 'drug_exposure' and 'concept_ancestor' table. The 'cdm'
+#' object must contain the 'write_schema' as attribute and  the user should have
 #' permission to write on it. It is a compulsory input, no default value is
 #' provided.
+#' @param conceptIds List or vector of drug concept ids that form the concept
+#' set(s). Each element of the list is instantiated as as different cohort.
+#' 'conceptIds' or 'conceptSetFolder' are compulsory inputs and one of them
+#' should be provided to instantiate the cohort(s).
 #' @param conceptSetFolder Relative path from the root of the project to the
 #' folder where the concept set .json files are contained. It will be
-#' instantiated one cohort for each .json file. It is a compulsory input, no
-#' default value is provided.
+#' instantiated one cohort for each .json file. 'conceptIds' or
+#' 'conceptSetFolder' are compulsory inputs and one of them should be provided
+#' to instantiate the cohort(s).
 #' @param gapEra Gap threshold between two consecutive exposures. If the number
 #' of non exposed days between two exposures is smaller or equal to 'gapEra'
 #' this non exposed days in between will be considered exposed. The input can be
@@ -34,7 +39,7 @@
 #' @param incidencePrevalenceCohortName Name of the table to be instantiated in
 #' the 'write_schema' of the database. By default:
 #' "incidence_prevalence_dus_tables".
-#' @param overWrite Weather or not overwrite the table if something is written
+#' @param overwrite Weather or not overwrite the table if something is written
 #' there. By default: FALSE.
 #' @param verbose Weather or not the code should print the steps of the process.
 #' By default: FALSE.
@@ -43,45 +48,70 @@
 #' table. The instantiated table is a cohort table in OMOP format
 #' (cohort_definition_id, subject_id, cohort_start_date, cohort_end_date). The
 #' individuals will be in the cohort while considered exposed by the concept
-#' set used to create that cohort. This function also provides a csv output with
+#' set used to create that cohort. This function also writes a csv file with
 #' the relation between the different cohort_definition_id, the name of the
 #' concept sets used and the gap between exposures considered. The file is named
-#' "equivalences.csv" in the same folder where the concept sets are.
+#' "equivalences.csv" and is written in the project origin (if no
+#' 'conceptSetFolder' is provided) or in the same folder where the concept sets
+#'  are.
 #'
 #' @export
 #'
 #' @examples
 instantiateIncidencePrevalenceCohorts <- function(cdm,
-                                                  conceptSetFolder,
+                                                  conceptIds = NULL,
+                                                  conceptSetFolder = NULL,
                                                   gapEra = 0,
                                                   incidencePrevalenceCohortName = "incidence_prevalence_dus_tables",
-                                                  overWrite = FALSE,
+                                                  overwrite = FALSE,
                                                   verbose = FALSE) {
   # CHECKS
   errorMessage <- checkmate::makeAssertCollection()
-  checkmate::assertClass(cdm, classes = "cdm_ref", add = errorMessage)
+  checkmate::assertClass(cdm, classes = "cdm_reference", add = errorMessage)
   # check that drug_exposure and concept tables exists is missing, to be
   # implemented when https://github.com/darwin-eu/CDMConnector/issues/28 is
   # adressed.
-  checkmate::assertPathForOutput(
-    here::here(conceptSetFolder, "equivalence.csv"),
-    overwrite = TRUE,
-    add = errorMessage
-  )
+  if (is.null(conceptSetFolder) && !is.null(conceptIds)){
+    if(!is.list(conceptIds)){
+      conceptIds <- list(conceptIds)
+    }
+    checkmate::assertList(
+      conceptIds,
+      min.len = 1,
+      add = errorMessage
+    )
+    checkmate::assertTRUE(
+      all(sapply(conceptIds, is.numeric)),
+      add = errorMessage
+    )
+    checkmate::assertTRUE(
+      all(sapply(conceptIds, function(x){sum(is.na(x)) == 0})),
+      add = errorMessage
+    )
+  } else if (!is.null(conceptSetFolder) && is.null(conceptIds)){
+    checkmate::assertPathForOutput(
+      here::here(conceptSetFolder, "equivalence.csv"),
+      overwrite = TRUE,
+      add = errorMessage
+    )
+  } else if (is.null(conceptSetFolder) && is.null(conceptIds)){
+    stop("'conceptSetFolder' or 'conceptIds' are compulsory inputs.")
+  } else {
+    stop("Use only 'conceptSetFolder' or 'conceptIds' as concept input.")
+  }
   checkmate::assertCount(gapEra, add = errorMessage)
   checkmate::assertCharacter(
     incidencePrevalenceCohortName,
     min.chars = 1, min.len = 1,
     add = errorMessage
   )
-  checkmate::assertLogical(overWrite, len = 1, add = errorMessage)
+  checkmate::assertLogical(overwrite, len = 1, add = errorMessage)
   checkmate::assertLogical(verbose, len = 1, add = errorMessage)
   checkmate::reportAssertions(collection = errorMessage)
 
   # check that you can write in the database and schema provided
 
-
-  # BLOCK 1 Obtain concept_set_names and cohort_definition_id
+  if (!is.null(conceptSetFolder)){
   # read the complete file name of the concepts in the specified folder
   conceptSets <- dplyr::tibble(concept_set_path = list.files(
     path = here::here(conceptSetFolder), full.names = TRUE
@@ -97,33 +127,34 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
     )
   # add the cohort_concept_id number
   conceptSets <- conceptSets %>%
-    dplyr::mutate(cohort_definition_id = dplyr::row_number())
+    dplyr::mutate(cohort_definition_id = as.character(dplyr::row_number()))
 
-  # BLOC 2 read the concept sets and obtain the drug_concept_id#
   # read concept sets and extract the list of included/excluded codes and
   # with or without descendants
   tryCatch(
     expr = conceptList <- readConceptSets(conceptSets),
-    error = function(e){
+    error = function(e) {
       stop("The json files are not properly formated OMOP concept sets.")
     }
   )
   # add descendants to the ones that they have include_descendants = TRUE
   conceptList <- conceptList %>%
-    dplyr::filter(include_descedants == FALSE) %>%
+    dplyr::filter(include_descendants == FALSE) %>%
     dplyr::union(
-      conceptList %>%
-        dplyr::filter(include_descedants == TRUE) %>%
+      cdm[["concept_ancestor"]] %>%
+        dplyr::select(
+          "concept_id" = "ancestor_concept_id",
+          "descendant_concept_id"
+        ) %>%
         dplyr::inner_join(
-          cdm[["concept"]] %>%
-            dplyr::select(
-              "concept_id" = "ancestor_concept_id",
-              "descendant_concept_id"
-            ),
+          conceptList %>%
+            dplyr::filter(include_descendants == TRUE),
+          copy = TRUE,
           by = "concept_id"
         ) %>%
         dplyr::select(-"concept_id") %>%
-        dplyr::rename("concept_id" = "descendant_concept_id")
+        dplyr::rename("concept_id" = "descendant_concept_id") %>%
+        dplyr::collect()
     ) %>%
     dplyr::select(-"include_descendants") %>%
     dplyr::rename("drug_concept_id" = "concept_id")
@@ -137,7 +168,20 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
       by = "drug_concept_id"
     )
 
-  # BLOC 3 subset drug exposure table
+  } else {
+
+    conceptList <- dplyr::bind_rows(
+      lapply(
+        conceptIds,
+        function(x) {
+          dplyr::tibble(drug_concept_id = x)
+        }
+      ),
+      .id = "cohort_definition_id"
+    )
+
+  }
+
   # select only the variables of interest
   incidencePrevalenceCohort <- cdm[["drug_exposure"]] %>%
     dplyr::select(
@@ -147,7 +191,7 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
       "drug_exposure_end_date"
     )
   # subset the drug exposure table
-  incidencePrevalenceCohort <- incidencePrevalenceCohort
+  incidencePrevalenceCohort <- incidencePrevalenceCohort %>%
     dplyr::inner_join(conceptList,
       by = "drug_concept_id",
       copy = TRUE
@@ -155,8 +199,8 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
     dplyr::select(-"drug_concept_id") %>%
     dplyr::compute()
 
-  # BLOC 4 obtain the start of the intervals where the person may be exposed
-  # An interval starts at the start of an exposure
+  # Obtain the start of the intervals where the person may be exposed. An
+  # interval starts at the start of an exposure
   incidencePrevalenceStart <- incidencePrevalenceCohort %>%
     dplyr::select("cohort_definition_id", "person_id",
       "interval_start_date" = "drug_exposure_start_date"
@@ -187,7 +231,8 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
   incidencePrevalenceStart <- incidencePrevalenceStart %>%
     dplyr::group_by(.data$cohort_definition_id, .data$person_id) %>%
     dplyr::filter(
-      .data$interval_start_date < max(.data$interval_start_date, na.rm = TRUE))
+      .data$interval_start_date < max(.data$interval_start_date, na.rm = TRUE)
+    )
   # we sort and number the starts of the intervals
   incidencePrevalenceStart <- incidencePrevalenceStart %>%
     dbplyr::window_order(.data$interval_start_date) %>%
@@ -195,27 +240,28 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
     dplyr::ungroup() %>%
     dplyr::compute()
 
-  # BLOC 5 obtain the end of the intervals where the person may be exposed
-  # A interval ends at the previous day when an exposure starts
+  # Obtain the end of the intervals where the person may be exposed. An interval
+  # ends at the previous day when an exposure starts
   incidencePrevalenceEnd <- incidencePrevalenceCohort %>%
     dplyr::select(
       "cohort_definition_id", "person_id",
       "drug_exposure_start_date"
     ) %>%
     dplyr::distinct() %>%
-    dplyr::mutate(cohort_end_date = as.Date(dbplyr::sql(
-      sql_add_days(
-        CDMConnector::dbms(attr(cdm, "dbcon")), -1,
+    dplyr::mutate(interval_end_date = as.Date(dbplyr::sql(
+      SqlUtilities::sql_add_days(
+        CDMConnector::dbms(attr(cdm, "dbcon")),
+        -1,
         "drug_exposure_start_date"
       )
     ))) %>%
-    dplyr::select(-"drug_exposure_start_date") %>%
+    dplyr::select(-"drug_exposure_start_date")
   # or at the end of an exposure
-    incidencePrevalenceEnd <- incidencePrevalenceEnd %>%
+  incidencePrevalenceEnd <- incidencePrevalenceEnd %>%
     dplyr::union(
       incidencePrevalenceCohort %>%
         dplyr::select("cohort_definition_id", "person_id",
-                      "interval_end_date" = "drug_exposure_end_date"
+          "interval_end_date" = "drug_exposure_end_date"
         )
     )
   # we eliminate repeated ends:
@@ -226,7 +272,8 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
   incidencePrevalenceEnd <- incidencePrevalenceEnd %>%
     dplyr::group_by(.data$cohort_definition_id, .data$person_id) %>%
     dplyr::filter(
-      .data$interval_end_date > min(.data$interval_end_date, na.rm = TRUE))
+      .data$interval_end_date > min(.data$interval_end_date, na.rm = TRUE)
+    )
   # we sort and number the ends of the intervals
   incidencePrevalenceEnd <- incidencePrevalenceEnd %>%
     dbplyr::window_order(.data$interval_end_date) %>%
@@ -234,7 +281,6 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
     dplyr::ungroup() %>%
     dplyr::compute()
 
-  # BLOC 6 obtain the exposed intervals
   # Start and end dates are joined with the exposures
   incidencePrevalenceCohort <- incidencePrevalenceStart %>%
     dplyr::inner_join(incidencePrevalenceEnd,
@@ -245,12 +291,22 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
     )
   # we mark the empty intervals as gaps
   incidencePrevalenceCohort <- incidencePrevalenceCohort %>%
-    dplyr::group_by(.data$cohort_definition_id, .data$person_id, .data$index) %>%
-    dplyr::mutate(gap = dplyr::if_else(sum(
+    dplyr::mutate(inside = dplyr::if_else(
       .data$drug_exposure_start_date <= .data$interval_start_date &
-        .data$drug_exposure_end_date >= .data$interval_end_date
-    ) == 0, 1, 0)) %>%
-    dplyr::ungroup() %>%
+        .data$drug_exposure_end_date >= .data$interval_end_date,
+      1,
+      0
+    )) %>%
+    dplyr::group_by(
+      .data$cohort_definition_id, .data$person_id, .data$index,
+      .data$interval_start_date, .data$interval_end_date
+    ) %>%
+    dplyr::summarise(
+      number_exposures = sum(.data$inside, na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    dplyr::mutate(gap = dplyr::if_else(.data$number_exposures > 0, 0, 1)) %>%
+    dplyr::select(-"number_exposures") %>%
     dplyr::compute()
   # gaps smaller than gapEra are not marked as gaps
   incidencePrevalenceCohort <- incidencePrevalenceCohort %>%
@@ -260,9 +316,10 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
         dplyr::filter(.data$gap == 1) %>%
         dplyr::mutate(gap = dplyr::if_else(
           dbplyr::sql(SqlUtilities::sqlDiffDays(
-            CDMConnector::dbms(attr(cdm,"dbcon")),
+            CDMConnector::dbms(attr(cdm, "dbcon")),
             "interval_start_date",
-            "interval_end_date")) + 1 <= .env$gapEra,
+            "interval_end_date"
+          )) + 1 <= .env$gapEra,
           0,
           1
         ))
@@ -272,11 +329,13 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
     dplyr::group_by(.data$person_id, .data$cohort_definition_id)
   # We arrange by interval_start_date and compute the era_id of each exposure
   incidencePrevalenceCohort <- incidencePrevalenceCohort %>%
-    dbplyr::window_order(.data$interval_start_date) %>%
+    dbplyr::window_order(.data$index) %>%
     dplyr::mutate(era_id = cumsum(.data$gap))
   # We summarize each era to obtain cohort_start_date and cohort_end_date
   incidencePrevalenceCohort <- incidencePrevalenceCohort %>%
-    dplyr::group_by(.data$person_id, .data$cohort_definition_id, .data$era_id) %>%
+    dplyr::group_by(
+      .data$person_id, .data$cohort_definition_id, .data$era_id
+    ) %>%
     dplyr::summarise(
       cohort_start_date = min(.data$interval_start_date, na.rm = TRUE),
       cohort_end_date = min(.data$interval_end_date, na.rm = TRUE),
@@ -284,23 +343,30 @@ instantiateIncidencePrevalenceCohorts <- function(cdm,
     )
   # Select the final variables and compute
   incidencePrevalenceCohort <- incidencePrevalenceCohort %>%
-    dplyr::select("cohort_definition_id", "subject_id" = "person_id",
-                  "cohort_start_date", "cohort_end_date") %>%
+    dplyr::select("cohort_definition_id",
+      "subject_id" = "person_id",
+      "cohort_start_date", "cohort_end_date"
+    ) %>%
     dplyr::compute()
 
-  # BLOC 7 instantiate the cohorts in the database as permanent tables
-  cdm <-
+  # Instantiate the cohorts in the database as permanent tables
+  cdm[[incidencePrevalenceCohortName]] <-
     SqlUtilities::computePermanent(
       incidencePrevalenceCohort,
       incidencePrevalenceCohortName,
       schema = attr(cdm, "write_schema"),
-      overwrite = overWrite
+      overwrite = overwrite
     )
 
-  # BLOC 8 write the equivalence.csv table in the output folder
+  # Write the equivalence.csv table in the output folder
+  if (is.null(conceptSetFolder)){
+    fileName <- here::here("equivalence.csv")
+  } else {
+    fileName <- here::here(conceptSetFolder, "equivalence.csv")
+  }
   write.csv(
     conceptSets,
-    file = here::here(conceptSetFolder, "equivalence.csv"),
+    file = fileName,
     row.names = FALSE
   )
 
