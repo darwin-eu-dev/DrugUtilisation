60deff0345cc4cac15a8f158835fc8702e0be038
catalamarti <marticatalasabate@gmail.com>
2022-12-08 12:09:59 +0000

    prepare valporate example

--- extras/valproate.R
+++ extras/valproate.R
@@ -17,9 +17,29 @@ db <- dbConnect(RPostgres::Postgres(),
 
 cdm <- CDMConnector::cdm_from_con(db, cdm_schema = "public")
 
-cohort <- generateDrugUtilisationCohort(
+indicationCohortSet <- CDMConnector::readCohortSet(here::here("inst", "indications_example"))
+
+cdm$dus <- generateDrugUtilisationCohort(
   cdm,
   ingredientConceptId = 745466,
   studyStartDate = as.Date("2010-01-01")
 )
+
+attrition_dus <- attr(cdm$dus, "attrition")
+
+indication <- getIndication(
+  cdm = cdm,
+  targetCohortName = "dus",
+  indicationCohortName = "indication_valproate",
+  indicationDefinitionSet = indicationCohortSet %>% select("indication_id" = "cohortId", "indication_name" = "cohortName"),
+  indicationGap = 7,
+  unknownIndicationTables = c("condition_occurrence")
+)
+
+stratas <- getStratification(
+  cdm,
+  "dus",
+  sex = "Female",
+  ageGroup = list(c(0, 24), c(25, 49), c(50, 74), c(75, 150)),
+  )
 
