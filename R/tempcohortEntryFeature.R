3299ff5da26340f43055eb7cfc4114f1fe7dd88f
catalamarti <marticatalasabate@gmail.com>
2022-12-12 23:11:38 +0000

    add binding and some refactoring to getOverlappingCohortSubjects

--- R/tempcohortEntryFeature.R
+++ R/tempcohortEntryFeature.R
@@ -14,16 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' Get subjects in cohort1 that are also in cohort2 at the same time.
+#' This subject if in a certain window the target cohort overlaps with the
+#' overlap cohort
 #'
 #' @param cdm CDMConnector CDM reference object
-#' @param targetCohortId id or list of ids, of the target cohort, including information we want to check our cohort against
-#' @param interestCohortId id or list of ids, of the cohort we are interested in checking the prior history of each patient
-#' @param targetCohortTable name of the target cohort table
-#' @param interestCohortTable name of the interest cohort table
-#' @param lookbackWindow lookback period window in days, a list consist of two numbers
+#' @param targetCohortTable name of the cohort that we want to add the
+#' overlapping
+#' @param targetCohortId id or vector of ids, of the target cohort
+#' @param overlapCohortTable name of the cohort that we want to check if
+#' overlaps with the target cohort
+#' @param overlapCohortId id or vector of ids, of the cohort we are interested
+#' in
+#' @param lookbackWindow lookback period window in days, a list consist of two
+#' numbers
 #'
-#' @return a tibble describing if the people in cohort1 where in cohort2 at that time (and some specified lookback period).
+#' @return it add a new column to target cohort for each id in overlap cohort.
+#' The column is numeric and can be 0 if no overlap is observed and 1 if overlap
+#' is oberved.
 #' @export
 #'
 #' @examples
@@ -48,114 +55,114 @@
 #'   cohort = cohort
 #' )
 #' getOverlappingCohortSubjects(
-#'   cdm = cdm, targetCohortId = "2",
+#'   cdm = cdm,
 #'   targetCohortTable = "cohort",
-#'   interestCohortId = "1",
-#'   interestCohortTable = "cohort", lookbackWindow = c(0, 180)
+#'   targetCohortId = 1,
+#'   overlapCohortTable = "cohort",
+#'   overlapCohortId = c(2, 3),
+#'   lookbackWindow = c(-180, 0)
 #' )
 #' }
 #'
-getOverlappingCohortSubjects <- function(cdm, targetCohortId, targetCohortTable,
-                                         interestCohortId, interestCohortTable, lookbackWindow) {
+getOverlappingCohortSubjects <- function(cdm,
+                                         targetCohortTable,
+                                         targetCohortId = NULL,
+                                         overlapCohortTable,
+                                         overlapCohortId = NULL,
+                                         lookbackWindow = 0) {
+  if (is.character(targetCohortId)) {
+    targetCohortId <- as.numeric(targetCohortId)
+  }
+  if (is.character(overlapCohortId)) {
+    overlapCohortId <- as.numeric(overlapCohortId)
+  }
   # checks
   errorMessage <- checkmate::makeAssertCollection()
-  if (!is.null(targetCohortId) &&
-    is.numeric(targetCohortId)) {
-    cohortId <- as.character(targetCohortId)
-  }
-  checkmate::assertCharacter(targetCohortId, add = errorMessage)
-  if (!is.null(interestCohortId) &&
-    is.numeric(interestCohortId)) {
-    cohortId <- as.character(interestCohortId)
-  }
-  checkmate::assertCharacter(interestCohortId, add = errorMessage)
-  checkmate::check_true(targetCohortTable %in% names(cdm))
-  checkmate::check_true(interestCohortTable %in% names(cdm))
-  checkmate::assertNumeric(lookbackWindow, len = 2, add = errorMessage)
+  checkmate::assertTRUE(targetCohortTable %in% names(cdm), add = errorMessage)
+  checkmate::assertIntegerish(
+    targetCohortId,
+    null.ok = TRUE, add = errorMessage
+  )
+  checkmate::assertTRUE(overlapCohortTable %in% names(cdm), add = errorMessage)
+  checkmate::assertIntegerish(
+    overlapCohortId,
+    null.ok = TRUE, add = errorMessage
+  )
+  checkmate::assertNumeric(
+    lookbackWindow, min.len = 1, max.len = 2, add = errorMessage
+  )
   checkmate::reportAssertions(collection = errorMessage)
 
-  cohort1 <- cdm[[targetCohortTable]] %>%
-    dplyr::filter(.data$cohort_definition_id %in% .env$targetCohortId) %>%
-    dplyr::rename("target_start_date" = "cohort_start_date") %>%
-    dplyr::rename("target_end_date" = "cohort_end_date")
+  if (length(lookbackWindow) == 1) {
+    lookbackWindow <- c(lookbackWindow, lookbackWindow)
+  }
 
+  checkmate::assertTRUE(lookbackWindow[1] <= lookbackWindow[2])
 
-  cohort2 <- cdm[[interestCohortTable]] %>%
-    dplyr::filter(.data$cohort_definition_id == .env$interestCohortId) %>%
-    dplyr::rename("interest_start_date" = "cohort_start_date") %>%
-    dplyr::rename("interest_end_date" = "cohort_end_date")
+  overlapCohort <- cdm[[overlapCohortTable]]
 
+  if (is.null(targetCohortId)) {
+    targetCohort <- cdm[[targetCohortTable]]
+  } else {
+    targetCohort <- cdm[[targetCohortTable]] %>%
+      dplyr::filter(.data$cohort_definition_id %in% .env$targetCohortId)
+  }
+  if (is.null(overlapCohortId)) {
+    overlapCohort <- cdm[[overlapCohortTable]]
+  } else {
+    overlapCohort <- cdm[[overlapCohortTable]] %>%
+      dplyr::filter(.data$cohort_definition_id %in% .env$overlapCohortId)
+  }
 
-  result <- cohort2 %>%
-    dplyr::left_join(cohort1, by = "subject_id") %>%
+  overlapCohort <- overlapCohort %>%
+    dplyr::rename(
+      "overlap_start_date" = "cohort_start_date",
+      "overlap_end_date" = "cohort_end_date",
+      "overlap_id" = "cohort_definition_id"
+    )
+
+  result <- targetCohort %>%
+    dplyr::left_join(overlapCohort, by = "subject_id") %>%
     dplyr::mutate(
-      overlap_end_date = as.Date(dbplyr::sql(CDMConnector::dateadd(
-        date = "interest_start_date",
-        number = !!-lookbackWindow[1]
-      ))),
       overlap_start_date = as.Date(dbplyr::sql(CDMConnector::dateadd(
-        date = "interest_start_date",
+        date = "overlap_start_date",
         number = !!-lookbackWindow[2]
+      ))),
+      overlap_end_date = as.Date(dbplyr::sql(CDMConnector::dateadd(
+        date = "overlap_end_date",
+        number = !!-lookbackWindow[1]
       )))
     ) %>%
-    dplyr::group_by(.data$subject_id, .data$cohort_definition_id.y) %>%
-    dplyr::mutate(
-      target_end_date_max = max(.data$target_end_date, na.rm = TRUE),
-      target_start_date_min = min(.data$target_start_date, na.rm = TRUE)
-    ) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(
-      indicator =
-        dplyr::case_when(
-          is.na(.data$target_start_date) ~ 0,
-          is.na(.data$target_end_date) ~ 0,
-          .data$target_end_date_max < .data$overlap_start_date ~ 0,
-          .data$target_start_date_min > .data$overlap_end_date ~ 0,
-          TRUE ~ 1
-        )
-    ) %>%
-    dplyr::rename(
-      "cohort_start_date" = "interest_start_date",
-      "cohort_end_date" = "interest_end_date",
-      "cohort_definition_id" = "cohort_definition_id.x",
-      "target_cohort_definition_id" = "cohort_definition_id.y"
+    dplyr::filter(
+      .data$cohort_start_date >= .data$overlap_start_date &
+        .data$cohort_start_date <= .data$overlap_end_date
     ) %>%
     dplyr::select(
-      "cohort_definition_id", "subject_id",
-      "cohort_start_date", "cohort_end_date",
-      "target_cohort_definition_id",
-      "indicator"
+      "cohort_definition_id", "subject_id", "cohort_start_date",
+      "cohort_end_date", "overlap_id"
     ) %>%
-    dplyr::mutate(across(all_of("target_cohort_definition_id"), as.numeric)) %>%
-    dplyr::compute()
-
-
-
-  resultNotInTarget <- result %>%
-    dplyr::filter(is.na(.data$target_cohort_definition_id)) %>%
-    dplyr::select(cohort_definition_id, subject_id, cohort_start_date, cohort_end_date)
-  resultInTarget <- result %>%
-    dplyr::mutate(targetCohortTableName = .env$targetCohortTable) %>%
-    dplyr::filter(!is.na(.data$target_cohort_definition_id)) %>%
-    tidyr::pivot_wider(
-      names_from = c(targetCohortTableName,target_cohort_definition_id),
-      values_from = indicator,
-      names_glue = "overlap_{targetCohortTableName}_{target_cohort_definition_id}"
-    )
-
-  result <- resultNotInTarget %>%
-    dplyr::full_join(resultInTarget, by = c(
-      "cohort_definition_id",
-      "subject_id", "cohort_start_date",
-      "cohort_end_date"
-    )) %>%
     dplyr::mutate(
-      across(starts_with("overlap"), ~ case_when(
-        is.na(.) ~ 0,
-        TRUE ~ .
-      ))
-    )
-
+      indicator = 1,
+      overlap_id = as.numeric(.data$overlap_id),
+      overlapCohortTableName = .env$overlapCohortTable
+    ) %>%
+    tidyr::pivot_wider(
+      names_from = c("overlapCohortTableName", "overlap_id"),
+      values_from = "indicator",
+      names_glue = "overlap_{overlapCohortTableName}_{overlap_id}",
+      values_fill = 0
+    ) %>%
+    dplyr::right_join(
+      targetCohort,
+      by = c(
+        "cohort_definition_id", "subject_id", "cohort_start_date",
+        "cohort_end_date"
+      )
+    ) %>%
+    dplyr::mutate(dplyr::across(
+      dplyr::starts_with("overlap"), ~ dplyr::if_else(is.na(.x), 0, .x)
+    )) %>%
+    dplyr::compute()
 
   return(result)
 }
