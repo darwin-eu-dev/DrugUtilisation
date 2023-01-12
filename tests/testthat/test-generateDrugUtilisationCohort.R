067ec6453eeb960b2c97ad1b1d4324d5505bc628
catalamarti <marticatalasabate@gmail.com>
2023-01-12 20:07:39 +0000

    Update test-generateDrugUtilisationCohort.R

--- tests/testthat/test-generateDrugUtilisationCohort.R
+++ tests/testthat/test-generateDrugUtilisationCohort.R
@@ -26,7 +26,7 @@ test_that("test inputs", {
   expect_error(generateDrugUtilisationCohort(
     cdm = cdm, ingredientConceptId = 1, summariseMode = "2020-01-05"
   ))
-  expect_error(generateDrugUtilisationCohort(
+  expect_no_error(generateDrugUtilisationCohort(
     cdm = cdm, ingredientConceptId = 1, summariseMode = "FixedTime"
   ))
   expect_error(generateDrugUtilisationCohort(
