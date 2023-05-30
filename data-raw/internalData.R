597bf8636c47959978d55d86df6649ec72d8d5eb
catalamarti <marticatalasabate@gmail.com>
2023-05-30 11:39:25 +0100

    pattern data

--- data-raw/internalData.R
+++ data-raw/internalData.R
@@ -30,7 +30,29 @@ domainInformation <- readr::read_csv(
   here::here("data-raw", "domain_information.csv"), show_col_types = FALSE
 )
 
+# add the current pattern file
+patternfile <- read_csv(
+  here::here("data-raw", "pattern_drug_strength.csv"),
+  col_types = list(
+    pattern_id = "numeric",
+    amount = "character",
+    amount_unit = "character",
+    amount_unit_concept_id = "numeric",
+    numerator = "character",
+    numerator_unit = "character",
+    denominator = "character",
+    denominator_unit = "character",
+    numerator_unit_concept_id = "numeric",
+    denominator_unit_concept_id = "numeric",
+    number_concepts = "numeric",
+    number_ingredients = "numeric",
+    valid = "logical",
+    pattern_name = "character",
+    unit = "character"
+  )
+)
+
 usethis::use_data(
   mockDrugStrength, mockConcept, mockConceptAncestor, domainInformation,
-  internal = TRUE, overwrite = TRUE
+  patternfile, internal = TRUE, overwrite = TRUE
 )
