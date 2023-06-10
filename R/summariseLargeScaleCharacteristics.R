64dfc18208169f53feca524f4f11e8cb8cb52c00
catalamarti <marticatalasabate@gmail.com>
2023-06-10 22:01:36 +0100

    first version of sum from codelist

--- R/summariseLargeScaleCharacteristics.R
+++ R/summariseLargeScaleCharacteristics.R
@@ -318,34 +318,164 @@ summariseCharacteristicsFromCodelist <- function(cohort,
     minCellCount = minCellCount
   )
 
+  # save cohortSet
+  cohortSet <- CDMConnector::cohortSet(cohort)
+
   # create codelist
   codelist <- codelistFromConceptSetList(conceptSetList)
 
   # identify domains
-  codelist <- addDomain(codelist)
+  codelist <- addDomain(codelist, cdm)
 
   # create subset
-  cohort <- conceptsSubset(cohort, cdm, codelist)
+  cohort <- conceptSubset(cohort, cdm, codelist, overlap)
 
   # count for each window
-  results <- NULL
-  for (k in seq_along(window)) {
-    results <- results %>%
-      dplyr::union_all(countOccurrences(cohort, window, strata))
-  }
-
-  # format in summariseReuslts
-  results <- formatSummary(results)
+  result <- countOccurrences(cohort, window, strata) %>%
+    dplyr::inner_join(cohortSet, by = "cohort_definition_id")
 
   # counts denominator
-  results <- results %>%
-    dplyr::union_all(countDenominator(cohort, window, strata))
+  result <- result %>%
+    dplyr::union_all(
+      countDenominator(cohort, window, strata) %>%
+        dplyr::inner_join(cohortSet, by = "cohort_definition_id")
+    )
 
-  return(results)
+  # format output
+  result %>%
+    dplyr::rename(
+      "variable" = "concept_set_name", "group_name" = "cohort_name"
+    ) %>%
+    dplyr::mutate(
+      variable_type = "binary", variable_level = as.character(NA),
+      estimate_type = "count", group_name = "Cohort name",
+      cdm_name = CDMConnector::cdmName(cdm),
+      generated_by = paste0(
+        "DrugUtilisation_", utils::packageVersion("DrugUtilisation"),
+        "_summariseCharacteristicsFromCodelist"
+      )
+    ) %>%
+    dplyr::select(
+      "group_name", "group_level", "strata_name", "strata_level", "window_name",
+      "variable", "variable_type", "variable_level", "estimate_type",
+      "estimate", "cdm_name", "generated_by"
+    )
+
+  return(result)
 }
 
 codelistFromConceptSetList <- function(conceptSetList) {
   conceptSetList %>%
     lapply(dplyr::as_tibble) %>%
-    dplyr::bind_rows(.id = "concept_set_name")
+    dplyr::bind_rows(.id = "concept_set_name") %>%
+    dplyr::rename("concept_id" = "value")
+}
+
+addDomain <- function(codelist, cdm) {
+  cdm[["concept"]] %>%
+    dplyr::select("concept_id", "domain_id") %>%
+    dplyr::inner_join(
+      codelist %>% dplyr::select("concept_set_name", "concept_id"),
+      by = "concept_id", copy = TRUE
+    ) %>%
+    CDMConnector::computeQuery()
+}
+
+conceptSubset <- function(cohort, cdm, codelist, overlap) {
+  domains <- unique(codelist$domain_id)
+  for (k in seq_along(domains)) {
+    domain <- domains[k]
+    tableName <- getDomainInfo(domain, "table_name")
+    conceptName <- getDomainInfo(domain, "concept_id_name")
+    startName <- getDomainInfo(domain, "start_name")
+    endName <- getDomainInfo(domain, "end_name")
+    codelist <- codelist %>%
+      dplyr::filter(.data$domain_id == .env$domain) %>%
+      dplyr::select("concept_set_name", !!conceptName := "concept_id")
+    x <- cdm[[tableName]] %>%
+      dplyr::select(
+        "subject_id" = "person_id",
+        "start_date" = dplyr::all_of(startName),
+        "end_date" = dplyr::all_of(ifelse(overlap, endName, startName)),
+        dplyr::all_of(conceptName)
+      ) %>%
+      dplyr::inner_join(cohort, by = "subject_id")
+    if (k == 1) {
+      subsetResult <- x
+    } else {
+      subsetResult <- dplyr::union_all(subsetResult, x)
+    }
+  }
+  subsetResult <- subsetResult %>%
+    dplyr::union_all(
+      cdm[["observation_period"]] %>%
+        dplyr::select(
+          "subject_id" = "person_id",
+          "start_date" = "observation_period_start_date",
+          "end_date" = "observation_period_end_date"
+        ) %>%
+        dplyr::mutate(concept_set_name = "denominator") %>%
+        dplyr::inner_join(cohort, by = "subject_id")
+    ) %>%
+    dplyr::mutate(end_date = dplyr::if_else(
+      is.na(.data$end_date), .data$start_date, .data$end_date
+    )) %>%
+    CDMConnector::computeQuery()
+  return(subsetResult)
+}
+
+getDomainInfo <- function(domain, column) {
+  domainInformation[[column]][domainInformation$domain_id == domain]
+}
+
+countOccurrences <- function(cohort, window, strata) {
+  for (k in seq_along(window)) {
+    windowStart <- window[[k]][1]
+    windowEnd <- window[[k]][2]
+    windowName <- names(window)[k]
+    cohortK <- cohort
+    if (!is.infinite(windowStart)) {
+      cohortK <- cohortK %>%
+        dplyr::filter(.data$end_date >= .data$cohort_start_date + !!windowStart)
+    }
+    if (!is.infinite(windowEnd)) {
+      cohortK <- cohortK %>%
+        dplyr::filter(.data$start_date <= .data$cohort_start_date + !!windowEnd)
+    }
+    cohortK <- cohortK %>%
+      dplyr::select(
+        "cohort_definition_id", "subject_id", "cohort_start_date",
+        "concept_set_name", dplyr::all_of(unique(unlist(strata)))
+      ) %>%
+      dplyr::distinct() %>%
+      CDMConnector::computeQuery()
+    resultK <- cohortK %>%
+      dplyr::group_by(.data$cohort_definition_id, .data$concept_set_name) %>%
+      dplyr::summarise(estimate = dplyr::n()) %>%
+      dplyr::collect() %>%
+      dplyr::mutate(strata_group = "Overall", strata_level = "Overall")
+    for (i in seq_along(strata)) {
+      resultK <- resultK %>%
+        dplyr::union_all(
+          cohortK %>%
+            tidyr::unite(
+              "strata_level", dplyr::all_of(strata[[i]]), sep = " and "
+            ) %>%
+            dplyr::group_by(
+              .data$cohort_definition_id, .data$concept_set_name,
+              .data$strata_level
+            ) %>%
+            dplyr::summarise(estimate = dplyr::n()) %>%
+            dplyr::collect() %>%
+            dplyr::mutate(strata_group = names(strata)[i])
+        )
+    }
+    resultK <- resultK %>%
+      dplyr::mutate(window_name = !!windowName)
+    if (k == 1) {
+      result <- resultK
+    } else {
+      result <- dplyr::union_all(result, resultk)
+    }
+  }
 }
