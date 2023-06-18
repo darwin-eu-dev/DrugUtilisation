e778a4cfc9661bc9cac50e2460fdbfa514475f6b
catalamarti <marticatalasabate@gmail.com>
2023-06-18 23:41:49 +0100

    explicit attrition

--- R/generateDrugUtilisationCohortSet.R
+++ R/generateDrugUtilisationCohortSet.R
@@ -135,25 +135,33 @@ generateDrugUtilisationCohortSet <- function(cdm,
     # require daysPriorHistory
     cohort <- requireDaysPriorHistory(cohort, cdm, daysPriorHistory)
     attrition <- computeCohortAttrition(
-      cohort, cdm, attrition, "daysPriorHistory applied"
+      cohort, cdm, attrition, paste0(
+        "at least ", daysPriorHistory, " prior history"
+      )
     )
 
     # require priorUseWashout
     cohort <- requirePriorUseWashout(cohort, cdm, priorUseWashout)
     attrition <- computeCohortAttrition(
-      cohort, cdm, attrition, "priorUseWashout applied"
+      cohort, cdm, attrition, paste0(
+        "prior use wahout of ", priorUseWashout, " days"
+      )
     )
 
     # trim start date
     cohort <- trimStartDate(cohort, cdm, cohortDateRange[1])
     attrition <- computeCohortAttrition(
-      cohort, cdm, attrition, "cohort_start_date >= cohort_date_range_start"
+      cohort, cdm, attrition, paste0(
+        "cohort_start_date >= ", cohortDateRange[1]
+      )
     )
 
     # trim end date
     cohort <- trimEndDate(cohort, cdm, cohortDateRange[2])
     attrition <- computeCohortAttrition(
-      cohort, cdm, attrition, "cohort_end_date <= cohort_date_range_end"
+      cohort, cdm, attrition, paste0(
+        "cohort_end_date <= ", cohortDateRange[2]
+      )
     )
 
     # apply summariseMode
