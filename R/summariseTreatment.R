626ff47b5d900ea8d064cf04a64e14433a772487
catalamarti <marticatalasabate@gmail.com>
2023-12-06 21:30:16 +0000

    Update summariseTreatment.R

--- R/summariseTreatment.R
+++ R/summariseTreatment.R
@@ -78,7 +78,7 @@ summariseTreatment<- function(cohort,
       PatientProfiles::addCohortIntersectFlag(
         targetCohortTable = treatmentCohortName,
         targetCohortId = treatmentCohortId,
-        targetEndDate = NULL,
+        #targetEndDate = NULL,
         window = window,
         nameStyle = "{window_name}_{cohort_name}"
       )
