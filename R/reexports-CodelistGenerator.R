057474495dd193fd5257d986e8a1b864fe606406
catalamarti <marticatalasabate@gmail.com>
2024-07-09 17:01:15 +0100

    add more codelistGenerator reexports

--- R/reexports-CodelistGenerator.R
+++ R/reexports-CodelistGenerator.R
@@ -6,3 +6,11 @@ CodelistGenerator::getDrugIngredientCodes
 #' @importFrom CodelistGenerator getATCCodes
 #' @export
 CodelistGenerator::getATCCodes
+
+#' @importFrom CodelistGenerator codesFromCohort
+#' @export
+CodelistGenerator::codesFromCohort
+
+#' @importFrom CodelistGenerator codesFromConceptSet
+#' @export
+CodelistGenerator::codesFromConceptSet
