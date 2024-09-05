9b82aac4b9cac8395b3882102341654c52bbfd2a
catalamarti <marticatalasabate@gmail.com>
2024-09-05 17:52:57 +0100

    Update cohortUtilities.R

--- R/cohortUtilities.R
+++ R/cohortUtilities.R
@@ -96,7 +96,7 @@ erafyCohort <- function(cohort, gap) {
     name <- omopgenerics::tableName(cohort)
     cohort <- cohort |>
       erafy(gap) |>
-      dplyr::compute(name) |>
+      dplyr::compute(name = name, temporary = FALSE) |>
       omopgenerics::recordCohortAttrition(paste(
         "join exposures separated by", gap, "or less days"
       ))
